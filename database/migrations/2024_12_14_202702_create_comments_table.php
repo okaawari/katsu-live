67bcf5263923eb94e246e92a37c9c9f17173951d
Okawari <noxious0510@gmail.com>
2024-12-14 20:48:32 +0800

    created migration for all tables

---
+++ database/migrations/2024_12_14_202702_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->integer('comment_id', true);
+            $table->integer('reply_id')->nullable();
+            $table->integer('anime_id');
+            $table->integer('user_id');
+            $table->text('comment');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
